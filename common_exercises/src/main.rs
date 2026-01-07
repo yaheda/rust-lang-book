732a03cff99de63339ee49c79390b83f7c8704a1
Abu Yaheda <yaheda.a@gmail.com>
2026-01-07 19:50:52 +0100

    Add temperature conversion, Fibonacci calculation, and Christmas song functions

--- common_exercises/src/main.rs
+++ common_exercises/src/main.rs
@@ -1,3 +1,43 @@
 fn main() {
     println!("Hello, world!");
+    let celcius = convert_to_celcius(68.0);
+    println!("68 Fahrenheit is {celcius} Celsius");
+    let fib = fibonacci(10);
+    println!("The 10th Fibonacci number is {fib}");
+    christmas_song();
+}
+
+fn convert_to_celcius(fahrenheit: f64) -> f64 {
+    (fahrenheit - 32.0) * 5.0 / 9.0
+}
+
+fn fibonacci(n: u32) -> u32 {
+    if n <= 1 {
+        return n;
+    }
+    fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+fn christmas_song() {
+    let gifts = [
+        "a partridge in a pear tree",
+        "two turtle doves",
+        "three french hens",
+        "four calling birds",
+        "five golden rings",
+        "six geese a laying",
+        "seven swans a swimming",
+        "eight maids a milking",
+        "nine ladies dancing",
+        "ten lords a leaping",
+        "eleven pipers piping",
+        "twelve drummers drumming",
+    ];
+    for day in 1..12 {
+        println!("On the {} day of Christmas my true love sent to me", day);
+        for line in (0..day).rev() {
+            println!("{}", gifts[line]);
+        }
+        println!("");
+    }
 }
