5b0fb0b993e0b9a12d0b2669182b9e1a4f032692
Abu Yaheda <yaheda.a@gmail.com>
2026-01-29 20:51:17 +0100

    Implement state existence checks for US states in quarters and enhance related functions

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -33,7 +33,36 @@ fn main() {
         7 => remove_fancy_hat(),
         num @ 1..=6 => move_player(num),
         _ => reroll(),
+        //_ => (),
     }
+
+    let config_max = Option::Some(3u8);
+    match config_max {
+        Option::Some(max) => println!("The max is configured to be {}", max),
+        _ => (),
+    }
+
+    let config_max2 = Option::Some(3u8);
+    if let Option::Some(max) = config_max2 {
+        println!("The max is configured to be {}", max);
+    }
+
+    let coin = Coin::Quarter(USState::Alabama);
+    let mut count = 0;
+    match &coin {
+        Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+        _ => count += 1,
+    }
+
+    let mut count2 = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count2 += 1;
+    }
+
+    let v1 = describe_state_in_quarter(Coin::Quarter(USState::Alabama));
+    println!("{:?}", v1);
 }
 
 fn add_fancy_hat() {}
@@ -55,6 +84,55 @@ enum USState {
     Alabama,
     Alaska,
 }
+
+impl USState {
+    fn existed_in_year(&self, year: i32) -> bool {
+        match self {
+            USState::Alabama => year >= 1819,
+            USState::Alaska => year >= 1859,
+        }
+    }
+}
+
+fn describe_state_in_quarter(coin: Coin) -> Option<String> {
+    if let Coin::Quarter(state) = coin {
+        if state.existed_in_year(1900) {
+            Option::Some(format!("{:?} existed in 1900", state))
+        } else {
+            Option::Some(format!("{:?} did not exist in 1900", state))
+        }
+    } else {
+        Option::None
+    }
+}
+
+fn describe_state_in_quarter2(coin: Coin) -> Option<String> {
+    let state = if let Coin::Quarter(state) = coin {
+        state
+    } else {
+        return Option::None;
+    };
+
+    if state.existed_in_year(1900) {
+        Option::Some(format!("{:?} existed in 1900", state))
+    } else {
+        Option::Some(format!("{:?} did not exist in 1900", state))
+    }
+}
+
+fn describe_state_in_quarter3(coin: Coin) -> Option<String> {
+    let Coin::Quarter(state) = coin else {
+        return Option::None;
+    };
+
+    if state.existed_in_year(1900) {
+        Option::Some(format!("{:?} existed in 1900", state))
+    } else {
+        Option::Some(format!("{:?} did not exist in 1900", state))
+    }
+}
+
+
 
 
 enum Coin {
@@ -112,6 +190,7 @@ impl Message {
     }
 }
 
+#[derive(Debug)]
 enum Option<T> {
     None,
     Some(T),
