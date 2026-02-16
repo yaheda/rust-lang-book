6b668e0595af41d3a7a812279bedda9920593057
Abu Yaheda <yaheda.a@gmail.com>
2026-02-16 21:12:21 +0100

    Add City and Color structs with Display implementations for formatted output

--- helloworld/main.rs
+++ helloworld/main.rs
@@ -1,4 +1,4 @@
-use std::fmt::{self, write};
+use std::fmt::{self, write, Formatter, Display};
 
 fn main() {
     println!("hello world");
@@ -99,6 +99,22 @@ fn main() {
     let v = List(vec![1, 2, 3]);
     println!("{}", v);
 
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
+    ].iter() {
+        println!("{}", *city);
+    }
+
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        println!("{}", *color);
+    }
+
 }
 
 #[derive(Debug)]
@@ -170,4 +186,35 @@ impl fmt::Display for List {
     }
 }
 
+struct City {
+    name: &'static str,
+    // Latitude
+    lat: f32,
+    // Longitude
+    lon: f32,
+}
+impl fmt::Display for City {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+        // `write!` is like `format!`, but it will write the formatted result
+        // into a string
+        write!(f, "{}: {:.3}°{} {:.3}°{}",
+            self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
+    }
+}
+
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "RGB ({}, {}, {}) 0x{0:02X}{1:02X}{2:02X}", self.red, self.green, self.blue)
+    }
+}
 
