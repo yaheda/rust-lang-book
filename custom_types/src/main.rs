b7fa5cb1c66a321c5569205671ff8f487e1f1578
Abu Yaheda <yaheda.a@gmail.com>
2026-02-22 18:26:30 +0100

    Implement linked list structure with prepend, length, and string representation methods; update main to demonstrate usage

--- custom_types/src/main.rs
+++ custom_types/src/main.rs
@@ -84,6 +84,57 @@ fn main() {
 
     println!("roses are #{:06x}", Color::Red as u32);
     println!("violets are #{:06x}", Color::Blue as u32);
+
+    println!("****************** Testcase linked list");
+    let mut list = List::new();
+    list = list.prepend(1);
+    list = list.prepend(2);
+    list = list.prepend(3);
+
+    println!("linked list has length: {}", list.len());
+    println!("{}", list.strigify());
+}
+
+use crate::List::*;
+
+enum List {
+    // Cons: Tuple struct that wraps an element and a pointer to the next node
+    Cons(u32, Box<List>),
+    // Nil: A node that signifies the end of the linked list
+    Nil,
+}
+impl List {
+    // Create an empty list
+    fn new() -> List {
+        Nil
+    }
+
+    // Consume a list, and return the same list with a new element at its front
+    fn prepend(self, elem: u32) -> List {
+        Cons(elem, Box::new(self))
+    }
+
+    // Return the length of the list
+    fn len(&self) -> u32 {
+        match *self {
+            // Can't take ownership of the tail, because `self` is borrowed;
+            // instead take a reference to the tail
+            Cons(_, ref tail) => 1 + tail.len(),
+            Nil => 0
+        }
+    }
+
+    // Return representation of the list as a (heap allocated) string
+    fn strigify(&self) -> String {
+        match *self {
+            Cons(head, ref tail) => {
+                format!("{}, {}", head, tail.strigify())
+            },
+            Nil => {
+                format!("Nil")
+            }
+        }
+    }
 }
 
 enum Number {
